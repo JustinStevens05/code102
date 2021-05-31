ed7632b128f6ac28831493d88108ebff329356ef
JustinStevens05 <83788210+JustinStevens05@users.noreply.github.com>
2021-05-31 10:00:16 -0500

    edits

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if (customerName!= null) {
 
 var r = confirm("Are you sure you typed your name right");
 
-var rating = 1
+//var rating = 1
 
 do{
     var rating = prompt("do you think this website will be helpful please rate from 1 to 10",);
