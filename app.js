89fd3dd14fa520dc2674e0da8a2e297075b2a293
JustinStevens05 <83788210+JustinStevens05@users.noreply.github.com>
2021-05-31 12:52:32 -0500

    stuff

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ if (customerName!= null) {
 var r = confirm("Are you sure you typed your name right");
 
 var rating = prompt("What would you rate the look of this website",);
-if (Number.isInteger(rating)) {
+if (rating!= null) {
     document.getElementById("rate").innerHTML =
     "Thanks for the information";
 }
