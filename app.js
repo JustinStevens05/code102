e922d31b47d15ad42044e338a06c54a3c92aea03
JustinStevens05 <83788210+JustinStevens05@users.noreply.github.com>
2021-05-31 12:15:49 -0500

    updated question

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ if (customerName!= null) {
 var r = confirm("Are you sure you typed your name right");
 
 var rating = prompt("What would you rate the look of this website",);
-var rating = prompt("do you think this website will be helpful?",);
-
-if (rating!= null) {
-
+if (Number.isInteger(rating)) {
     document.getElementById("rate").innerHTML =
-
     "Thanks for the information";
-    "When asked if you thought the website would be helpful you said " + rating;
 }
