24273df17b05864508b28a84b35ec724be1d877f
JustinStevens05 <83788210+JustinStevens05@users.noreply.github.com>
2021-06-02 16:58:49 -0500

    combined two questions

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-/* var customerName = prompt("Please enter your name",);
-if (customerName!= null) {
-    document.getElementById("welcome").innerHTML =
-    "Hello " + customerName + "! How are you today?";
+var r = false
+while(r= false){
+    var customerName = prompt("Please enter your name",);
+    if (customerName!= null) {
+        document.getElementById("welcome").innerHTML =
+        "Hello " + customerName + "! How are you today?";
+    }
+    var r = confirm("Are you sure you typed your name right");
 }
-
-var r = confirm("Are you sure you typed your name right");
 
 var rating = prompt("What would you rate the look of this website",);
 if (rating!= null) {
     document.getElementById("rate").innerHTML =
     "Thanks for the information";
 }
-*/
