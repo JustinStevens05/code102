095d9d2b14df7251402859d16ab17ef005af4d49
JustinStevens05 <83788210+JustinStevens05@users.noreply.github.com>
2021-07-10 19:16:16 -0500

    changed image source

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ while(namecheck=== "no"){
 for(howMany = prompt("how many images would you like"); howMany > 0; howMany -= 1){
     var img = document.createElement("img"); 
  
-    img.src = 90539_0.jpg
+    img.src = https://4.bp.blogspot.com/-knTMC27r2DI/WEEBIXuL_gI/AAAAAAAACzo/ebc-C9ONNncTIX3INe9HxfuHeK9dcDTAACLcB/s1600/90539_0.jpg
     var src = document.getElementById("pic"); 
  
     src.appendChild(img); 
