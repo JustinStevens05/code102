e526e4fc5876aec4a22733fd7313211a3abdafdd
JustinStevens05 <83788210+JustinStevens05@users.noreply.github.com>
2021-08-20 17:26:45 -0500

    made most things into functions and added isNaN

--- app.js
+++ app.js
@@ -2,36 +2,34 @@ function double(number){
     return number * 2;
 }
 
-var namecheck = "no";
-while(namecheck=== "no"){
-    var visitorName = prompt("Please enter your name");
-    if (visitorName!== "") {
-        document.getElementById("welcome").innerHTML =
-        "Hello " + visitorName + " welome to my website.";
+function enterName{
+    var namecheck = "no";
+    while(namecheck=== "no"){
+        var visitorName = prompt("Please enter your name");
+        if (visitorName!== "") {
+            document.getElementById("welcome").innerHTML =
+            "Hello " + visitorName + " welome to my website.";
+        }
+        namecheck = prompt("Did you spell your name right?");
     }
-    namecheck = prompt("Did you spell your name right?");
 }
 
-
-for(howMany = prompt("how many images would you like"); howMany > 0; howMany -= 1){
-    var img = document.createElement("img"); 
+function images{
+    for(howMany = prompt("how many images would you like"); howMany > 0; howMany -= 1){
+        var img = document.createElement("img"); 
  
-    img.src = "https://4.bp.blogspot.com/-knTMC27r2DI/WEEBIXuL_gI/AAAAAAAACzo/ebc-C9ONNncTIX3INe9HxfuHeK9dcDTAACLcB/s1600/90539_0.jpg";
-    var src = document.getElementById("pic"); 
+        img.src = "https://4.bp.blogspot.com/-knTMC27r2DI/WEEBIXuL_gI/AAAAAAAACzo/ebc-C9ONNncTIX3INe9HxfuHeK9dcDTAACLcB/s1600/90539_0.jpg";
+        var src = document.getElementById("pic"); 
  
-    src.appendChild(img); 
+        src.appendChild(img); 
+    }
 }
+    
 
-var isItANumber = true;
 var petNumberLoop = 2;
 while(petNumberLoop != 1){
     var numberOfPets = prompt("How many pets is to many");
-    if(numberOfPets !== 1){
-        isItANumber = true;
-    } else{
-        isItANumber = false;
-    }    
-    if(isItANumber = true;){
+    if(isNaN(numberOfPets)= true){
         numberOfPets = double(numberOfPets);
         document.getElementById("number").innerHTML =
         "I think you should get " + numberOfPets + " pets";
